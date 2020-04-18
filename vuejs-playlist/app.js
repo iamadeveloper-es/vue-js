8ce7ca195ac8265157b1843b179c547cd61a73c1
Daniel <verdudaniel7@gmail.com>
2020-04-18 11:21:51 +0200

    Data Binding

--- vuejs-playlist/app.js
+++ vuejs-playlist/app.js
@@ -2,6 +2,8 @@ new Vue({
     el: '#vue-app',
     data: {
         appTitle: 'I am a Developer Learning Vue',
+        website: 'http://iamadeveloper.es',
+        printSpan: '<span>This is an example of how to <strong>output an "html tag"</strong> using data binding.</span>'
 
     },
     methods:{
